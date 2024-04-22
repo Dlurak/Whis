61e08df085689e509e4d197abbfd6481ef17c603
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-04-22 21:18:09 +0200

    Fix the url regex

--- src/lib/constants/regex.ts
+++ src/lib/constants/regex.ts
@@ -1,7 +1,5 @@
 /**
  * Regex to match a url
  * This is global (so it has the g flag at the end)
- * https://urlregex.com/
  */
-export const URL_REGEX =
-	/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+export const URL_REGEX = /https?:\/\/\S+/g
